75bc4c1b3c4533ab5edc836cb866877b1b637eab
Inesa Kostova <kostovainesa@gmail.com>
2025-02-18 16:04:01 +0200

    Add files via upload

---
+++ Personal Titles.cs
@@ -0,0 +1,23 @@
+﻿double ages = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+if (gender == 'm')
+{if (ages < 16)
+    {
+        Console.WriteLine("Master");
+    }
+    else
+    {
+        Console.WriteLine("Mr.");
+    }
+}
+else if (gender =='f')
+{ if (ages < 16)
+    {
+        Console.WriteLine("Miss");
+    } 
+else
+    {
+        Console.WriteLine("Ms.");
+    }
+ }
